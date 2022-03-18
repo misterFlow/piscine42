3fc85c595e4ce4893121f25ea8cb606704a92793
misterFlow <florian.guy82@gmail.com>
2022-03-18 15:04:25 +0100

    yyy

---
+++ C04/test/ex01/ft_putstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fguy <marvin@42lausanne.ch>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/26 14:29:39 by fguy              #+#    #+#             */
+/*   Updated: 2021/07/27 20:08:25 by fguy             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+void	ft_putstr(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}	
+}
+
+/*int	main(void)
+{
+	char	s1[] = "salut";
+	char	s2[] = "";
+
+	ft_putstr(s1);
+	ft_putstr(s2);
+	return (0);
+}*/
